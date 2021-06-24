89708a2221ca93fff273fd64679206e34b547dd6
Lyle Haldin <hankel343@gmail.com>
2021-06-24 15:36:42 -0500

    Redesigned how relationships work between entities. Also reflected in an updated diagram of foreign keys.

--- DataInsertion.sql
+++ DataInsertion.sql
@@ -7,3 +7,23 @@ WITH
 		ROWTERMINATOR = '\n'
 	)
 
+/* Inserting data into the Departments table */
+INSERT INTO Departments VALUES(100,'College of Liberal Arts','231 Ross',142519864);
+INSERT INTO Departments VALUES(101,'College of Engineering','2215 Coover Hall',254099823);
+INSERT INTO Departments VALUES(102,'College of Business','1200 Gerdin Business Building',248965255);
+
+/* Inserting data into the Insructors table */
+INSERT INTO Instructors VALUES(142519864,100,.75);
+INSERT INTO Instructors VALUES(142519864,101,.25);
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
+INSERT INTO Instructors VALUES();
